f3a39b063ae2f23bc0c4de197fe9c788e1493a30
Fangjun Kuang <csukuangfj@gmail.com>
2023-07-20 17:24:08 +0800

    add windows x86_64

---
+++ sherpa_onnx/sherpa_onnx_windows.go
@@ -0,0 +1,39 @@
+//go:build (windows && amd64) || (windows && 386)
+
+package sherpa_onnx
+
+import (
+	sherpa "github.com/k2-fsa/sherpa-onnx-go-windows/sherpa-onnx"
+)
+
+type OnlineTransducerModelConfig = sherpa.OnlineTransducerModelConfig
+type FeatureConfig = sherpa.FeatureConfig
+type OnlineRecognizerConfig = sherpa.OnlineRecognizerConfig
+type OnlineRecognizerResult = sherpa.OnlineRecognizerResult
+type OnlineRecognizer = sherpa.OnlineRecognizer
+type OnlineStream = sherpa.OnlineStream
+
+var NewOnlineRecognizer = sherpa.NewOnlineRecognizer
+var DeleteOnlineRecognizer = sherpa.DeleteOnlineRecognizer
+
+var NewOnlineStream = sherpa.NewOnlineStream
+var DeleteOnlineStream = sherpa.DeleteOnlineStream
+
+// ============================================================
+// For offline ASR (i.e., non-streaming ASR)
+// ============================================================
+type OfflineTransducerModelConfig = sherpa.OfflineTransducerModelConfig
+type OfflineParaformerModelConfig = sherpa.OfflineParaformerModelConfig
+type OfflineNemoEncDecCtcModelConfig = sherpa.OfflineNemoEncDecCtcModelConfig
+type OfflineLMConfig = sherpa.OfflineLMConfig
+type OfflineModelConfig = sherpa.OfflineModelConfig
+type OfflineRecognizerConfig = sherpa.OfflineRecognizerConfig
+type OfflineRecognizer = sherpa.OfflineRecognizer
+type OfflineStream = sherpa.OfflineStream
+type OfflineRecognizerResult = sherpa.OfflineRecognizerResult
+
+var NewOfflineRecognizer = sherpa.NewOfflineRecognizer
+var DeleteOfflineRecognizer = sherpa.DeleteOfflineRecognizer
+
+var NewOfflineStream = sherpa.NewOfflineStream
+var DeleteOfflineStream = sherpa.DeleteOfflineStream
